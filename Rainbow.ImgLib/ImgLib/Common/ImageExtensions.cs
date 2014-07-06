e565b0a8eae0309a8331b098f2f92ee25421fbb5
marco-calautti <marco.calautti@gmail.com>
2014-07-06 21:29:07 +0200

    stupid VS2013...

---
+++ Rainbow.ImgLib/ImgLib/Common/ImageExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Rainbow.ImgLib.Common
+{
+    public static class ImageExtensions
+    {
+        public static Color[] GetColorArray(this Image img)
+        {
+            Bitmap bmp = new Bitmap(img);
+            var list = new List<Color>();
+            for (int x = 0; x < img.Width; x++)
+                for (int y = 0; y < img.Height; y++)
+                    list.Add(bmp.GetPixel(x, y));
+            return list.ToArray();
+        }
+    }
+}
---
+++ Rainbow.ImgLib/ImgLib/Formats/Serializers/TextureFormatSerializerProvider.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Rainbow.ImgLib.Formats.Serializers
+{
+    /// <summary>
+    /// This class helps creating the right TextureFormatSerializer for a given source texture. The texture can be given in stream form or file form.
+    /// Given a file extension, it is also possible to request a serializer of a texture format whose preferred file extension if the given one.
+    /// User made serializers can be made available at runtime by means of the static method RegisterSerializer.
+    /// </summary>
+    public static class TextureFormatSerializerProvider
+    {
+        private static List<TextureFormatSerializer> serializers = new List<TextureFormatSerializer>();
+
+        static TextureFormatSerializerProvider()
+        {
+            RegisterSerializer(new TIM2TextureSerializer());
+        }
+
+        /// <summary>
+        /// Adds the given serializer to the list of available TextureFormatSerializers.
+        /// </summary>
+        /// <param name="serializer"></param>
+        public static void RegisterSerializer(TextureFormatSerializer serializer)
+        {
+            serializers.Add(serializer);
+        }
+
+        /// <summary>
+        /// Retrieves a serializer for textures of the given file format extension.
+        /// </summary>
+        /// <param name="formatExtension"></param>
+        /// <returns>the requested serializer if found, null otherwise.</returns>
+        public static TextureFormatSerializer FromFileFormatExtension(string formatExtension)
+        {
+            foreach (var serializer in serializers)
+                if (serializer.PreferredFormatExtension == formatExtension)
+                    return serializer;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Retrieves a serializer for textures of the given metadata file extension
+        /// </summary>
+        /// <param name="metadataExtension"></param>
+        /// <returns>the requested serializer if found, null otherwise.</returns>
+        public static TextureFormatSerializer FromFileMetadataExtension(string metadataExtension)
+        {
+            foreach (var serializer in serializers)
+                if (serializer.PreferredMetadataExtension == metadataExtension)
+                    return serializer;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Retrieves a serializer for textures encoded in the same format of the given stream. The given stream can be either a file format stream or a metadata stream.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns>the requested serializer if found, null otherwise.</returns>
+        public static TextureFormatSerializer FromStream(Stream stream)
+        {
+            foreach (var serializer in serializers)
+                if (serializer.IsValidFormat(stream) || serializer.IsValidMetadataFormat(stream))
+                    return serializer;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Retrieves a serializer for textures encoded in the same format of the given file. The given file can either encode a texture in metadata or original form.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns>the requested serializer if found, null otherwise.</returns>
+        public static TextureFormatSerializer FromFile(string filePath)
+        {
+            using (Stream s = File.Open(filePath, FileMode.Open))
+                return FromStream(s);
+        }
+    }
+}
