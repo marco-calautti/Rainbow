f1f7a0d541d7ab0b0c99e0cdbebecacbace821b3
marco-calautti <marco.calautti@gmail.com>
2014-09-17 15:51:22 +0200

    Updated copyright snippet.

--- Rainbow.ImgLib/ImgLib/Formats/Serialization/Metadata/XmlMetadataWriter.cs
+++ Rainbow.ImgLib/ImgLib/Formats/Serialization/Metadata/XmlMetadataWriter.cs
@@ -1,4 +1,4 @@
-﻿//Copyright (C) 2014 Marco (Phoenix) Calautti.
+﻿//Copyright (C) 2014+ Marco (Phoenix) Calautti.
 
 //This program is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
