9d436f4114cc1af7bba314ccaee881f1fe7062e8
marco-calautti <marco.calautti@gmail.com>
2015-08-27 23:50:57 +0200

    Added copyright snippet and minor fixes.

--- Rainbow.ImgLib/ImgLib/Formats/Implementation/TPLTexture.Serializer.cs
+++ Rainbow.ImgLib/ImgLib/Formats/Implementation/TPLTexture.Serializer.cs
@@ -1,4 +1,21 @@
-﻿using Rainbow.ImgLib.Common;
+﻿//Copyright (C) 2014+ Marco (Phoenix) Calautti.
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, version 2.0.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License 2.0 for more details.
+
+//A copy of the GPL 2.0 should have been included with the program.
+//If not, see http://www.gnu.org/licenses/
+
+//Official repository and contact information can be found at
+//http://github.com/marco-calautti/Rainbow
+
+using Rainbow.ImgLib.Common;
 using Rainbow.ImgLib.Encoding;
 using Rainbow.ImgLib.Encoding.Implementation;
 using Rainbow.ImgLib.Filters;
