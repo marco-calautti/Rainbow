0dbd7c0fe8395baa6cef6b97c06e2437b6825eff
marco-calautti <marco.calautti@gmail.com>
2017-07-13 17:58:24 +0200

    Reduced Target Framework to 4.0.

--- Rainbow.App/Properties/AssemblyInfo.cs
+++ Rainbow.App/Properties/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Runtime.InteropServices;
 
 
 [assembly: Guid("f45955b8-7532-490d-9298-59a0390b43cf")]
-[assembly: AssemblyInformationalVersion("v1.01-55-gdd47aaf")]
+[assembly: AssemblyInformationalVersion("v1.01-56-g0fb2998")]
