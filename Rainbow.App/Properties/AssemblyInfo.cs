592ed20297767a2c8d810133c8ee009a7e682857
marco-calautti <marco.calautti@gmail.com>
2017-07-18 15:03:57 +0200

    Version update.

--- Rainbow.App/Properties/AssemblyInfo.cs
+++ Rainbow.App/Properties/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Runtime.InteropServices;
 
 
 [assembly: Guid("f45955b8-7532-490d-9298-59a0390b43cf")]
-[assembly: AssemblyInformationalVersion("v1.01-85-gb7a9afd")]
+[assembly: AssemblyInformationalVersion("v1.01-88-g6961431")]
