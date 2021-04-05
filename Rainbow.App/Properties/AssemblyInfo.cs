3970ca3ffe9c0aa3ae55d27c2a25b172cf47183c
marco-calautti <marco.calautti@gmail.com>
2021-04-05 11:38:02 +0200

    Fixed travis

--- Rainbow.App/Properties/AssemblyInfo.cs
+++ Rainbow.App/Properties/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Runtime.InteropServices;
 
 
 [assembly: Guid("f45955b8-7532-490d-9298-59a0390b43cf")]
-[assembly: AssemblyInformationalVersion("1.2.1-3")]
+[assembly: AssemblyInformationalVersion("1.2.1-3-2-gc0074f6")]
